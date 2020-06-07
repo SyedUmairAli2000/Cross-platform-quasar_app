5d53b0c9fa783daf5d42fface8791989131367c4
Syed Umair Ali <syedumairali.617@gmail.com>
2020-06-07 16:07:02 +0500

    initial commit

---
+++ src/pages/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">About</h5>
+    <p>This app is cross-platform app built using the Quasar framework it can be used on almost on platforms <br>
+      1) Android (Cordova) <br>
+      2) Ios (Cordova) <br>
+      3) Web app <br>
+      4) SSR <br>
+      5) Desktop app (Linux/Windows/MacOS) natively using electron framework
+    </p>
+    <p>Quasar is a great framework for building cross-platform apps it uses vue.js as it's front-end and it is very functionl</p>
+     <br>
+    <p>You can see all of my other projects on Github at @SyedUmairAli2000 </p>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ src/pages/Help.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>This app is cross-platform app built using the Quasar framework it can be used on almost on platforms <br>
+      1) Android (Cordova) <br>
+      2) Ios (Cordova) <br>
+      3) Web app <br>
+      4) SSR <br>
+      5) Desktop app (Linux/Windows/MacOS) natively using electron framework
+    </p>
+    <p>This app does not have a back-end which means it is not connected to any database so your data won't be saved. This is not a production grade app this is something that i made just for fun.</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
